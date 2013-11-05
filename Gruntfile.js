fb35a5043abbac2766c7e574c518d204cce822eb
lilyxue <xll2651@gmail.com>
2013-11-05 14:47:55 +0800

    更改端口为9999

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 9999,
                 livereload: 35729,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
