5c1bc5a55d7792554e0dacac332b4145a00e8196
Amio <amio.cn@gmail.com>
2013-11-12 18:00:20 +0800

    引入AngularJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,7 +320,7 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'jshint',
+        //'jshint',
         'test',
         'build'
     ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('toHELL')
+  .controller('MainCtrl', function ($scope) {
+    $scope.abc = '123';
+  });
