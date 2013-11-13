0ddf7048b0957dbbb3b894b64a3f450da63929c5
Amio <amio.cn@gmail.com>
2013-11-13 17:51:02 +0800

    WA功能：添加项目数据文件（$scopr.project）

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,25 @@
 
 angular.module('toHELL')
   .controller('MainCtrl', function ($scope) {
-    $scope.abc = '123';
+    $scope.project = {
+      name: 'Demo 1',
+      scenes: [
+        {
+          id: 'Scene 1',
+          background: '',
+          elements: [
+            {
+              type: 'button',
+              size: [80, 30],  // width, height
+              position: [0,0], // x, y
+              actions: {
+                jumpto: 'Scene 2',
+                transition: '',
+                direction: 'left'
+              }
+            }
+          ]
+        }
+      ]
+    };
   });
