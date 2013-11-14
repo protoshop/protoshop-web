6fda73ab7a01d042c5f9401fc5f9ca1b9c8e3de0
Amio <amio.cn@gmail.com>
2013-11-14 16:12:41 +0800

    WA功能：完成左栏数据和视图的同步

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,49 @@
 
 angular.module('toHELL')
   .controller('MainCtrl', function ($scope) {
+    $scope.projectStat = {
+      selectedOrder: 0
+    };
     $scope.project = {
       name: 'Demo 1',
       scenes: [
         {
-          id: 'Scene 1',
+          order: 0,
+          name: 'Scene 1',
+          background: 'images/zzz-scene-thumb.png',
+          elements: [
+            {
+              type: 'button',
+              size: [80, 30],  // width, height
+              position: [0,0], // x, y
+              actions: {
+                jumpto: 'Scene 2',
+                transition: '',
+                direction: 'left'
+              }
+            }
+          ]
+        },
+        {
+          order: 1,
+          name: 'Scene 2',
+          background: 'images/zzz-scene-thumb.png',
+          elements: [
+            {
+              type: 'button',
+              size: [80, 30],  // width, height
+              position: [0,0], // x, y
+              actions: {
+                jumpto: 'Scene 2',
+                transition: '',
+                direction: 'left'
+              }
+            }
+          ]
+        },
+        {
+          order: 2,
+          name: 'Scene 3',
           background: '',
           elements: [
             {
