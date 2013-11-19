a512737437d32ab752095715384c3dd7533f6d70
Amio <amio.cn@gmail.com>
2013-11-19 17:09:03 +0800

    WA功能：列表页功能完成（样式待调整）

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@ angular.module('toHELL')
     };
     $scope.package = {
       appName: 'Demo HELL1',
-      appIcon: '',
+      appIcon: 'images/icon-app-120.png',
       splash: {
         image: 'splash.png',
         delay: 500,
@@ -18,7 +18,6 @@ angular.module('toHELL')
         {
           order: 0,
           name: 'Scene 1',
-          icon: 'images/icon-app-120.png',
           background: 'images/zzz-scene-thumb.png',
           elements: [
             {
@@ -59,6 +58,68 @@ angular.module('toHELL')
       $scope.editStat.selectedScene = scene.order;
     };
   }])
-  .controller('PackageListCTRL', ['$scope', function ($scope) {
-    $scope.aaa = 'adsf';
+  .controller('PackageListCTRL', ['$scope', '$location', function ($scope, $location) {
+    $scope.packageList = [
+      {
+        packageName: 'Jade',
+        packageIcon: 'images/icon-app-120.png',
+        modifyDate: '2013-10-27',
+        changelog: [
+          {
+            by: 'guolei',
+            note: '改了些颜色',
+            date: '2013-10-24'
+          }
+        ]
+      },
+      {
+        packageName: 'Jade',
+        packageIcon: 'images/icon-app-120.png',
+        modifyDate: '2013-10-27',
+        changelog: []
+      },
+      {
+        packageName: 'Mike',
+        packageIcon: 'images/icon-app-120.png',
+        modifyDate: '2013-10-27',
+        changelog: [
+          {
+            by: 'guolei',
+            note: '改了些颜色',
+            date: '2013-10-24'
+          },
+          {
+            by: 'guolei',
+            note: '又改了些颜色',
+            date: '2013-10-24'
+          },
+          {
+            by: 'guolei',
+            note: '最喜欢改颜色了',
+            date: '2013-10-24'
+          }
+        ]
+      },
+      {
+        packageName: 'Jade',
+        packageIcon: 'images/icon-app-120.png',
+        modifyDate: '2013-10-27',
+        changelog: [
+          {
+            by: 'guolei',
+            note: 'I see your true color.',
+            date: '2013-10-24'
+          }
+        ]
+      },
+    ];
+
+    $scope.editPackage = function (pkg) {
+      console.log(pkg);
+      $location.path('/package');
+    }
+
+    $scope.deletePackage = function(pkg){
+      console.log(pkg);
+    }
   }]);
