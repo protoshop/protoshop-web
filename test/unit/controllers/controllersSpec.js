a3db95efd1436551116015c41276fdf09c9f0561
Yanghe Liu <liuyanghejerry@126.com>
2013-12-02 15:24:40 +0800

    WA代码：修正测试代码

--- test/unit/controllers/controllersSpec.js
+++ test/unit/controllers/controllersSpec.js
@@ -1,36 +1,23 @@
-describe("Unit: Testing Controllers", function() {
+describe('Unit: Testing Controllers', function() {
 
-  beforeEach(angular.mock.module('toHELL'));
-  var toHELL = angular.module('toHELL');
+  beforeEach(module('toHELL'));
 
-  it('should have a PackageEditCTRL controller', function() {
-    expect(toHELL.PackageEditCTRL).not.to.equal(null);
+  describe('Unit: Testing PackageEditCTRL', function() {
+    var scope = {};
+    var PackageEditCTRL = null;
+
+    it('should have a PackageEditCTRL controller', inject(function($controller) {
+      var PackageEditCTRL = $controller('PackageEditCTRL', {
+        $scope: scope
+      });
+      expect(PackageEditCTRL).not.to.equal(null);
+    }));
+
+    it('should have a editStat working in PackageEditCTRL controller',
+      inject(function($rootScope) {
+        expect(scope.editStat).not.to.equal(undefined);
+    }));
+
   });
-
-  it('should have a PackageListCTRL controller', function() {
-    expect(toHELL.PackageListCTRL).not.to.equal(null);
-  });
-
-  it('should have a properly working PackageEditCTRL controller', 
-    inject(function($rootScope, $controller/*, $httpBackend*/) {
-    var $scope = $rootScope.$new();
-    var ctrl = $controller('PackageEditCTRL', {
-      $scope : $scope
-      // $routeParams : {
-      //   q : searchTestAtr
-      // }
-    });
-  }));
-
-  it('should have a properly working PackageListCTRL controller', 
-    inject(function($rootScope, $controller/*, $httpBackend*/) {
-    var $scope = $rootScope.$new();
-    var ctrl = $controller('PackageListCTRL', {
-      $scope : $scope
-      // $routeParams : {
-      //   id : searchID
-      // }
-    });
-  }));
 
 });
