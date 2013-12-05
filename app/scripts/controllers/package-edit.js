49cca099322143a39a855fd29ae3d6979a952666
Amio <amio.cn@gmail.com>
2013-12-05 11:34:29 +0800

    WA代码：获取项目JSON的API对接成功。 配合 #32

--- app/scripts/controllers/package-edit.js
+++ app/scripts/controllers/package-edit.js
@@ -65,9 +65,7 @@ angular.module('toHELL')
        * @var {Object} $scope.package
        */
 //      $http.get('/api/package/' + $routeParams.pkgId + '.json')
-      $http.post(GLOBAL.apiHost + 'fetchProject/', {
-        appid: $routeParams.pkgId
-      })
+      $http.get(GLOBAL.apiHost + 'fetchProject/?appid=' +$routeParams.pkgId)
         .success(function (data) {
           $scope.package = data;
           sceneService.setPackage($scope.package);
