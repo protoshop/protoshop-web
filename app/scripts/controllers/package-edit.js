56b561af2bb3331f219433b0c412c775ca7f3afa
Yanghe Liu <liuyanghejerry@126.com>
2013-12-11 18:11:11 +0800

    WA功能：在新增场景之后自动聚焦至名称编辑框

--- app/scripts/controllers/package-edit.js
+++ app/scripts/controllers/package-edit.js
@@ -40,9 +40,15 @@ angular.module('toHELL')
       packageService.setStat($scope.editStat);
 
       $scope.addScene = function() {
-        sceneService.addScene();
+        var newOne = sceneService.addScene();
         elementService.deselectElement();
         actionService.deselectAction();
+        sceneService.selectScene(newOne);
+        // NOTE: 由于angular的HTML刷新是在整个函数运行结束之后，这里需要用异步的方式来延后得到新增的HTML
+        setTimeout(function() {
+          scrollToScene(newOne);
+          triggerSceneNameEditor(newOne);
+        }, 0);
       };
       $scope.removeScene = function(scene) {
         elementService.deselectElement();
@@ -136,6 +142,29 @@ angular.module('toHELL')
           'width=420,height=230,resizable,scrollbars=no,status=1,left=' + x + ',top=' + y
         );
       };
+
+      // WARN: 注意，这里和UI结合的过于紧密，在UI的改造中极易失效
+      function scrollToScene(scene) {
+        if(!(scene && scene.id)) {
+          return;
+        }
+
+        var item = angular.element('.scene-item[data-scene-id="' + scene.id + '"] input');
+        if(item.length) {
+          item[0].scrollIntoView();
+        }
+      }
+
+      // WARN: 注意，这里和UI结合的过于紧密，在UI的改造中极易失效
+      function triggerSceneNameEditor(scene) {
+        if(!(scene && scene.id)) {
+          return;
+        }
+
+        var jq = angular.element;
+        var item = jq('.scene-item[data-scene-id="' + scene.id + '"] input');
+        item.focus();
+      }
 
       /**
        * 保存编辑好的项目JSON数据
