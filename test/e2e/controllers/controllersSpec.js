cd6202b1fde8dc56330d0d8d3f381ecb3b3c4d0e
Yanghe Liu <liuyanghejerry@126.com>
2013-12-13 11:25:05 +0800

    WA测试：重新使端对端测试生效

--- test/e2e/controllers/controllersSpec.js
+++ test/e2e/controllers/controllersSpec.js
@@ -4,7 +4,7 @@ describe('E2E: Testing Controllers:', function() {
     browser().navigateTo('/');
   });
 
-  var packageName = 'cb5947eebab907988c0a158a99237781';
+  var packageName = 'cd1b1f5f09cce72183e0470e26673b21'; // NOTE: 服务器应当保证此包始终存在且不被随意修改
 
   describe('PackageEditCTRL', function() {
 
@@ -18,47 +18,33 @@ describe('E2E: Testing Controllers:', function() {
     });
 
     it('should have a settings panel shown when no scene is selected', function() {
-      expect(element('.tools-box.ng-hide').count()).toBe(4);
+      expect(element('.tools-box:not(.ng-hide)').count()).toBe(1);
+      expect(element('.tools-box:not(.ng-hide) .tools-setting').count()).toBe(1);
     });
 
     it('should have settings panel hidden when any scene is selected', function() {
       element('.layout-scenes').click();
-      expect(element('.tools-box.ng-hide').count()).toBe(4);
       element('.scene-item:first').click();
-      expect(element('.tools-box.ng-hide').count()).toBe(3);
+      expect(element('.tools-box:not(.ng-hide) .tools-setting').count()).toBe(0);
     });
 
-    it('should have hotspot shown only when first scene is selected', function() {
-      element('.scene-item:eq(3)').click();
-      expect(element('.actor.hotspot').count()).toBe(0);
-      element('.scene-item:eq(0)').click();
+    it('should have hotspot shown when second scene is selected', function() {
+      element('.scene-item:eq(1)').click();
       expect(element('.actor.hotspot').count()).toBe(1);
     });
 
     it('should have anchor shown only when hotspot is selected', function() {
-      element('.scene-item:eq(0)').click();
+      element('.scene-item:eq(1)').click();
       expect(element('.anchor.ng-hide').count()).toBe(1);
       element('.actor.hotspot:eq(0)').click();
       expect(element('.actor.ng-hide').count()).toBe(0);
     });
 
     it('should have goto sign shown only when hotspot is selected', function() {
-      element('.scene-item:eq(0)').click();
+      element('.scene-item:eq(2)').click();
       element('.actor.hotspot:eq(0)').click();
       expect(element('.scene-goto').count()).toBe(1);
     });
-
-    // it('should show the correct value in Position panel', function() {
-    //   element('.scene-item:eq(0)').click();
-    //   element('.actor.hotspot:eq(0)').click();
-      
-    //   element('.actor.hotspot:eq(0)').query(function (ele, done) {
-    //     var xInDevice = ele.position().left;
-    //     var x = element('.tools-position input:eq(0)').val();
-    //     expect(x).toEqual(xInDevice);
-    //     done();
-    //   });      
-    // });
 
 
   });
