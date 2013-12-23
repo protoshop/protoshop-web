5cb62f503061791f8c781965afbb8a59af70fdd3
Amio <amio.cn@gmail.com>
2013-12-23 14:55:50 +0800

    WA功能：添加上传ajax测试

---
+++ app/scripts/services/formdata.js
@@ -0,0 +1,13 @@
+
+/**
+ * Transform data to FormData
+ */
+angular.module('toHELL').factory('formDataObject', function() {
+  return function(data) {
+    var fd = new FormData();
+    angular.forEach(data, function(value, key) {
+      fd.append(key, value);
+    });
+    return fd;
+  };
+});
