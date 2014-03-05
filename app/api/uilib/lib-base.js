d02057a2a5de58691ad487eccbb79c0f19f997d1
Amio <amio.cn@gmail.com>
2014-03-05 11:04:38 +0800

    WA接口：更新 uilib 基础配置格式

---
+++ app/api/uilib/lib-base.js
@@ -0,0 +1,101 @@
+module.exports = [
+  {
+    "type": "button",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "backgroundColor": "",
+    "backgroundImage": "",
+    "alpha": 1,
+    "roundradius": 0,
+    "highlight": "imgurl",
+    "disabled": "imgurl",
+    "title": "I am a button",
+    "actions": [
+      {
+        "type": "jumpto",
+        "target": 1386064607748,
+        "transitionType": "push",
+        "transitionDirection": "up",
+        "transitionDelay": 0,
+        "transitionDuration": 0
+      }
+    ]
+  },
+  {
+    "type": "textfield",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "backgroundColor": "",
+    "keyboard": "ANSI", // number/email/url/telephone
+    "isPassword": false,
+    "font_color": "",
+    "font_size": 12,
+    "font_style": "LU", //  L/B/U/I
+    "placeholder": "Input Something"
+  },
+  {
+    "type": "imageview",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86
+  },
+  {
+    "type": "label",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "backgroundColor": "",
+    "font_color": "",
+    "font_size": 12,
+    "font_style": "LU" //  L/B/U/I
+  },
+  {
+    "type": "bar",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "backgroundColor": "",
+    "backgroundImage": "",
+    "title": "",
+    "titleColor": "",
+    "elements": [
+      {
+        "type": "button"
+      }
+    ]
+  },
+  {
+    "type": "scrollview",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "contentWidth": 200,
+    "contentHeight": 200,
+    "elements": []
+  },
+  {
+    "type": "hotspot",
+    "posX": 12,
+    "posY": 202,
+    "width": 139,
+    "height": 86,
+    "actions": [
+      {
+        "type": "jumpto",
+        "target": 1386064607748,
+        "transitionType": "push",
+        "transitionDirection": "up",
+        "transitionDelay": 0,
+        "transitionDuration": 0
+      }
+    ]
+  }
+];
---
+++ app/api/uilib/lib-core.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    "type": "view",
+    "posX": 0,
+    "posY": 0,
+    "width": 120,
+    "height": 44,
+    "backgroundColor": "",
+    "backgroundImage": "",
+    "alpha": 1,
+    "roundradius": 0
+  }
+];
