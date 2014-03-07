14259b66c09b43097354f88737cdb66e649b91be
Amio <amio.cn@gmail.com>
2014-03-07 17:30:30 +0800

    WA功能：更新 searchUser 接口WebApp端

--- app/scripts/services/modals.js
+++ app/scripts/services/modals.js
@@ -1,33 +1,65 @@
 'use strict';
 
 angular.module('toHELL')
-  .factory('dialogShare', ['btfModal', function(Modal){
-    return Modal({
-      controller: 'DialogShareCtrl',
-      controllerAs: 'modal',
-      templateUrl: 'partials/dialog-share.html'
-    })
-  }])
-  .controller('DialogShareCtrl', [
-    'dialogShare',
-    '$scope',
-    'GLOBAL',
-    '$http',
-    function(dialogShare, $scope, GLOBAL, $http){
-      
+.factory('dialogShare', ['btfModal', function (Modal) {
+  return Modal({
+    controller: 'DialogShareCtrl',
+    controllerAs: 'modal',
+    templateUrl: 'partials/dialog-share.html'
+  })
+}])
+.controller('DialogShareCtrl', [
+  'dialogShare',
+  '$scope',
+  'GLOBAL',
+  '$http',
+  'loginService',
+  function (dialogShare, $scope, GLOBAL, $http, loginService) {
+
     this.closeMe = dialogShare.deactivate;
-      
-    $scope.fellows = [];
-    $scope.lookup = '';
-    $scope.lookupFellows = function(){
-      if($scope.lookup === ''){
-        $scope.fellows = [];
-      }else{
-        $http.get(GLOBAL.apiHost + 'searchUser/?keyword=' + $scope.lookup)
-          .success(function(res){
-            $scope.fellows = res.results;
-          });
+    angular.element('.share-input').focus();
+
+    $scope.sharedFellows = [];
+    $http.get(GLOBAL.apiHost + 'sharelist/')
+
+    $scope.hiFellows = [];
+    $scope.lookupKeyword = '';
+
+    $scope.lookupFellows = function () {
+      if ($scope.lookupKeyword === '') {
+        $scope.hiFellows = [];
+      } else {
+        $http.post(GLOBAL.apiHost + 'searchUser/', {
+          token: loginService.getLoggedInUser().token,
+          appid: $scope.appID,
+          keyword: $scope.lookupKeyword
+        })
+        .success(function (res) {
+          $scope.hiFellows = res.results;
+        });
       }
     };
-      
-  }]);
+
+    $scope.addFellow = function (fellow) {
+
+      $http.post(GLOBAL.apiHost + 'share/', {
+        token: loginService.getLoggedInUser().token,
+        appid: $scope.appID,
+        user: fellow.email,
+        option: '1'
+      })
+      .success(function (res) {
+        switch (res.status) {
+        case '1':
+//          $scope.fellows = res.results;
+          console.log(res.results);
+          $scope.lookupKeyword = '';
+          break;
+        default:
+          var errDesc = GLOBAL.errDesc[res.error_code] || '未知错误';
+          console.log('添加分享伙伴 Error: ', errDesc, res);
+        }
+      });
+    }
+  }
+]);
