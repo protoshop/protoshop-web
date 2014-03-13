cb5813dc4b1863fc2455caaf09104edd5f341cb6
Amio <amio.cn@gmail.com>
2014-03-13 13:50:38 +0800

    WA修正：解决注册问题

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,23 +1,34 @@
 'use strict';
 
 angular.module('toHELL')
-  .controller('RegisterCTRL', ['$scope', '$location', '$http', 'GLOBAL', 'loginService',
-    function ($scope, $location, $http, GLOBAL, loginService) {
-      $scope.doSignup = function () {
-        $http.post(GLOBAL.apiHost + 'register/', $scope.user)
-          .success(function (res) {
-            switch (res.status) {
-            case '1':
-              loginService.doLogin($scope.user, function () {
-                $location.path('/list/');
-              });
-              break;
-            default:
-              var errDesc = GLOBAL.errDesc[res.error_code] || '未知错误';
-              alert(errDesc);
-              console.log('Signup Error: ', errDesc, res);
-            }
-          })
-          .error(GLOBAL.errLogger);
-      }
-    }]);
+.controller('RegisterCTRL', ['$scope', '$location', '$http', 'GLOBAL', 'loginService',
+  function ($scope, $location, $http, GLOBAL, loginService) {
+    $scope.doSignup = function () {
+
+      // 将密码做 MD5 转换
+      var account = {
+        email: $scope.user.email,
+        passwd: V.Security.md5($scope.user.passwd)
+      };
+
+      // 注册
+      $http.post(GLOBAL.apiHost + 'register/', account)
+      .success(function (res) {
+        switch (res.status) {
+        case '1':
+          // 注册成功后
+          // 以用户原始输入的账号信息进行自动登陆
+          loginService.doLogin($scope.user, function () {
+            $location.path('/list/');
+          });
+          break;
+        default:
+          var errDesc = GLOBAL.errDesc[res.error_code] || '未知错误';
+          alert(errDesc);
+          console.log('Signup Error: ', errDesc, res);
+        }
+      })
+      .error(GLOBAL.errLogger);
+    }
+  }
+]);
