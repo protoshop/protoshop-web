244f5d848c1b5ed778ed9d08980769be245d2936
Amio <amio.cn@gmail.com>
2014-04-18 15:59:51 +0800

    WA功能：添加UI控件的视觉渲染组件

---
+++ app/scripts/directives/scene-element-view.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('toHELL')
+
+/**
+ * Element（界面元素控件） in scene editor
+ */
+
+.directive('elementView', function(){
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      elemData: '&elemData'
+    },
+    templateUrl: 'partials/scene-element-view.html',
+    link: function(scope, el){
+      scope.elem = scope.elemData();
+      console.log(scope);
+    }
+  }
+});
