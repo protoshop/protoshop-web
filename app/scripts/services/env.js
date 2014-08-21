77601fe3b8ee0a211e7312d31bebdbef934c8d4c
杨飞 <yfdezhuye@163.com>
2014-08-21 14:41:36 +0800

    add module roadmap

--- app/scripts/services/env.js
+++ app/scripts/services/env.js
@@ -2,44 +2,33 @@
 
 angular.module('toHELL').service('ENV', function () {
 
-  var env = 'unknown';
-  
-  if (/debug|localhost/.test(window.location.href)) {
-    env = 'debug';
-  } else if (/ctripqa/.test(window.location.href)) {
-    env = 'ctqa';
-  } else {
-    env = 'open';
-  }
+    var env = 'unknown';
 
-  var apiHosts = {
-<<<<<<< HEAD
-    // debug: 'http://10.2.254.48/debugProtoShop/',
-    debug: 'http://api.protoshop.io/',
-=======
-    //debug: 'http://10.2.254.48/debugProtoShop/',
-    debug : 'http://api.protoshop.io/',
->>>>>>> 5263d9c1f64c706f0aebcd557cccfc041c6e1a21
-    ctqa: 'http://10.2.254.48/ProtoShop/',
-    open: 'http://api.protoshop.io/'
-  };
-  
-  var pkgRoots = {
-<<<<<<< HEAD
-    // debug: 'http://10.2.254.48/debugpackages/',
-     debug: 'http://protoshop.io/packages/',
+    if (/debug|localhost/.test(window.location.href)) {
+        env = 'debug';
+    } else if (/ctripqa/.test(window.location.href)) {
+        env = 'ctqa';
+    } else {
+        env = 'open';
+    }
 
-=======
-    //debug: 'http://10.2.254.48/debugpackages/',
-    debug: 'http://protoshop.io/packages/',
->>>>>>> 5263d9c1f64c706f0aebcd557cccfc041c6e1a21
-    ctqa: 'http://10.2.254.48/packages/',
-    open: 'http://protoshop.io/packages/'
-  };
+    var apiHosts = {
+        //debug: 'http://10.2.254.48/debugProtoShop/',
+        debug : 'http://api.protoshop.io/',
+        ctqa: 'http://10.2.254.48/ProtoShop/',
+        open: 'http://api.protoshop.io/'
+    };
 
-  return {
-    env: env,
-    apiHost: apiHosts[env],
-    pkgRoot: pkgRoots[env]
-  }
+    var pkgRoots = {
+        //debug: 'http://10.2.254.48/debugpackages/',
+        debug: 'http://protoshop.io/packages/',
+        ctqa: 'http://10.2.254.48/packages/',
+        open: 'http://protoshop.io/packages/'
+    };
+
+    return {
+        env: env,
+        apiHost: apiHosts[env],
+        pkgRoot: pkgRoots[env]
+    }
 });
