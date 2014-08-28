9c852098990d5e6bfd259be65ed78c86ec91f0bb
yangfei <yangfei@MioMac.local>
2014-08-28 10:53:59 +0800

    format code

--- app/scripts/directives/scene-element-view.js
+++ app/scripts/directives/scene-element-view.js
@@ -1,15 +1,12 @@
 'use strict';
-
 angular.module('toHELL')
-
 /**
  * Element（界面元素控件） in scene editor
  */
-
-.directive('elementView', function () {
-  return {
-    restrict: 'AE',
-    replace: true,
-    templateUrl: 'partials/scene-element-view.html'
-  };
+.directive('elementView', function() {
+        return {
+                restrict: 'AE',
+                replace: true,
+                templateUrl: 'partials/scene-element-view.html'
+        };
 });
