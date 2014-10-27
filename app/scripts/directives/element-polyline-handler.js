e1c03c60394df4c50046974bf9e5556b22bcce49
muyi <yfdezhuye@163.com>
2014-10-27 16:46:58 +0800

    add type polyline

--- app/scripts/directives/element-polyline-handler.js
+++ app/scripts/directives/element-polyline-handler.js
@@ -9,14 +9,16 @@ angular.module('toHELL')
             replace: 'true',
             templateUrl: 'partials/element-polyline-handler.html',
             link: function (scope, el) {
-                console.log(el.parent());
-                el.on('drag dragstart dragmove', function($ev){
+
+                var anchor, child;
+
+                el.on('drag dragstart dragmove', function ($ev) {
                     $ev.stopPropagation();
                     $ev.preventDefault();
-                   return false;
+                    return false;
                 });
 
-                el.parent().on('drag dragstart dragmove', function($ev){
+                el.parent().on('drag dragstart dragmove', function ($ev) {
                     $ev.stopPropagation();
                     $ev.preventDefault();
                     return false;
@@ -27,6 +29,8 @@ angular.module('toHELL')
                     if ($event.which !== 1) {
                         return;
                     }
+                    anchor = el.find('i');
+                    child = scope.elem.elements && scope.elem.elements[0];
                     // 记录初始状态
                     scope.origin = {
                         elemx: scope.elem.posX,
@@ -38,42 +42,60 @@ angular.module('toHELL')
                     };
                     scope.direction = $event.target.dataset.handle;
                     // 绑定
-
-                    if ($event.shiftKey){
-                        $document.one('mousemove', addNewLine);
-                    }else{
-                        $document.on('mousemove', updateElemRect);
-                    }
+                    $document.on('mousemove', updateElemRect);
                     $document.one('mouseup', unbindDragEvents);
                     $event.stopPropagation();
+                });
+
+                el.on('click', addNewLine);
+
+                el.on('mouseover', function ($event) {
+                    if ($event.shiftKey && (!scope.elem.elements || scope.elem.elements.length == 0)) {
+                        el.find('i').css('cursor', 'pointer');
+                    }
+                });
+
+                el.on('mouseout', function(){
+                    el.find('i').css('cursor', '');
                 });
 
                 /**
                  * 添加新折线
                  */
-                function addNewLine($ev){
-                    scope.elem.elements = scope.elem.elements || [];
-                    var type;
+                function addNewLine($ev) {
+                    if ($ev.shiftKey && (!scope.elem.elements || scope.elem.elements.length == 0)) {
+                        el.off('click', addNewLine);
+                        scope.elem.elements = scope.elem.elements || [];
+                        var type, elem = scope.elem, etype = elem.type, X=0, Y=0;
 
-                    switch (scope.elem.type){
-                        case 'hbefore':
-                            type = 'vbefore';
-                            break;
-                        case 'vbefore':
-                            type = 'hbefore';
-                            break;
-                        case 'hafter':
-                            type = 'vafter';
-                            break;
-                        case 'vafter':
-                            type = 'hafter';
-                            break;
-                    }
+                        switch (etype) {
+                            case 'hbefore':
+                                type = 'vbefore';
+                                X = elem.posX < 0 ? 0 : elem.width;
+                                Y = -20;
+                                break;
+                            case 'vbefore':
+                                type = 'hbefore';
+                                Y = elem.posY < 0 ? 0 : elem.height;
+                                X = -20;
+                                break;
+                            case 'hafter':
+                                type = 'vafter';
+                                X = elem.posX < 0 ? 0 : elem.width;
+                                break;
+                            case 'vafter':
+                                type = 'hafter';
+                                Y = elem.posY < 0 ? 0 : elem.height;
+                                break;
+                        }
 
-                    uilib.then(function(libs){
-                        var element = JSON.parse(JSON.stringify(libs.data[type].init));
+                        uilib.then(function (libs) {
+                            var element = JSON.parse(JSON.stringify(libs.data[type].init));
+                            element.posX = X;
+                            element.posY = Y;
                             scope.elem.elements.push(element);
-                    });
+                        });
+                    }
                 }
 
                 /**
@@ -81,25 +103,60 @@ angular.module('toHELL')
                  * @param $ev
                  */
                 function updateElemRect($ev) {
+                    $ev.stopPropagation();
                     var deltaX = $ev.clientX - scope.origin.mousex;
                     var deltaY = $ev.clientY - scope.origin.mousey;
-                    var wrapperSize = scope.elemData ? scope.elemData().contentSize : scope.$parent.$parent.size;
+                    var Y,X;
 
-
+                    /**
+                     * v:纵向  h:横向  bf:before  af:after
+                     */
                     switch (scope.direction) {
                         case 'v-bf':
-                            scope.elem.posY = scope.origin.elemy + deltaY;
-                            scope.elem.height = scope.origin.elemh - deltaY;
-                            break;
                         case 'v-af':
-                            scope.elem.height = scope.origin.elemh + deltaY;
+                            if (scope.origin.elemy >= 0){
+                                Y = scope.origin.elemh + deltaY;
+                            }else if(scope.origin.elemy < 0){
+                                Y = -1 * (scope.origin.elemh - deltaY);
+                            }
+
+                            if (Y>=0){
+                                scope.elem.posY = 0;
+                                anchor.css('top', '100%');
+                                if (child){
+                                    child.posY = Y;
+                                }
+                            }else{
+                                scope.elem.posY = Y;
+                                anchor.css('top', 0);
+                                if (child){
+                                    child.posY = 0;
+                                }
+                            }
+                            scope.elem.height = Math.abs(Y);
                             break;
                         case 'h-bf':
-                            scope.elem.posX = scope.origin.elemx + deltaX;
-                            scope.elem.width = Math.abs(scope.origin.elemw - deltaX);
-                            break;
                         case 'h-af':
-                            scope.elem.width = scope.origin.elemw + deltaX;
+                            if (scope.origin.elemx >= 0){
+                                X = scope.origin.elemw + deltaX;
+                            }else if(scope.origin.elemx < 0){
+                                X = -1 * (scope.origin.elemw - deltaX);
+                            }
+
+                            if (X>=0){
+                                scope.elem.posX = 0;
+                                anchor.css('left', '100%');
+                                if (child){
+                                    child.posX = X;
+                                }
+                            }else{
+                                scope.elem.posX = X;
+                                anchor.css('left', 0);
+                                if (child){
+                                    child.posX = 0;
+                                }
+                            }
+                            scope.elem.width = Math.abs(X);
                             break;
                     }
 
