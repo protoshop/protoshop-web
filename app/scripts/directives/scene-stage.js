4e3a5ad0414afe2589cac3ee7f71df3030ef07c2
muyi <yfdezhuye@163.com>
2014-11-10 13:55:04 +0800

    fixed polyline bugs

--- app/scripts/directives/scene-stage.js
+++ app/scripts/directives/scene-stage.js
@@ -9,23 +9,25 @@ angular.module('toHELL').directive('sceneStage', function ($rootScope, uilib, ed
             // Handle Event 'scene.addElement'
             scope.$on('scene.addElement', function (event, args) {
                 uilib.then(function (lib) {
-                    var type = args.type, isComment = type === 'notes';
+                    var type = args.type, isComment = type === 'notes',
+                        // 辅助工具可以拖动到手机舞台外面
+                        isAssists = 'vline line polyline notes'.split(' ').indexOf(type) >= 0;
                     var newElement = JSON.parse(JSON.stringify(lib.data[type].init));
-                    newElement.posX = isComment ? parseInt(args.posx - newElement.width * 0.5 - 101)
+                    newElement.posX = isAssists ? parseInt(args.posx - newElement.width * 0.5 - 101)
                         : parseInt(args.posx - 60).crop(0, 200);
-                    newElement.posY = isComment ? parseInt(args.posy - newElement.height * 0.5 - 142)
+                    newElement.posY = isAssists ? parseInt(args.posy - newElement.height * 0.5 - 142)
                         : parseInt(args.posy - 22).crop(0, 436);
 
-                    if (isComment){
+                    /*if (isComment){
                         newElement['cid'] = 'c_' + (+new Date());
-                    }
+                    }*/
 
                     var host = args.wrapper.elem || scope.editStat.selectedScene;
                     host.elements = host.elements || [];
                     host.elements.push(newElement);
-                    if (isComment) {
+                    /*if (isComment) {
                         scope.editStat.comments = editService.getComments(scope.editStat.selectedScene);
-                    }
+                    }*/
                 });
             });
 
